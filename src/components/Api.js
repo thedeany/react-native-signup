accb0d98811b2fee50d1b7e0c64398021ea856b0
Brandon Dean <>
2017-06-27 17:35:59 -0400

    Change API config to object instead of class
    This will need some work in the future

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,5 +1,37 @@
-class Api {
-	static url = 'https://demozonepublic.evanced.info/api/signup/eventlist?isOngoingVisible=true&isSpacesReservationVisible=true&onlyRegistrationEnabled=false&onlyFeaturedEvents=false';
+// config
+const apiConfig = {
+	spacesReservations: false,
+	ongoingEvents: true,
+	onlyEventsWithRegistration: false,
+	onlyFeaturedEvents: false,
+	ageGroups: [
+		// age group IDs
+	],
+	eventTypes: [
+		// event type IDs
+	]
+}
+
+// helpers
+ageGroups = () => {
+	if (apiConfig.ageGroups.length > 0) {
+		console.log(apiConfig.ageGroups.map(group => `ageGroups=${group}`).join('&') + '&')
+		return apiConfig.ageGroups.map(group => `ageGroups=${group}`).join('&') + '&';
+	}
+}
+
+eventTypes = () => {
+	if (apiConfig.eventTypes.length > 0) {
+		return apiConfig.eventTypes.map(type => `eventsTypes=${type}`).join('&') + '&';
+	}
+}
+
+
+
+// export
+const Api = {
+	url: `https://demozonepublic.evanced.info/api/signup/eventlist?${ageGroups()}${eventTypes()}isOngoingVisible=${apiConfig.ongoingEvents}&isSpacesReservationVisible=${apiConfig.spacesReservations}&onlyRegistrationEnabled=${apiConfig.onlyEventsWithRegistration}&onlyFeaturedEvents=${apiConfig.onlyFeaturedEvents}`,
+	config: apiConfig,
 }
 
 export default Api;
