f697bd8eb16d3e1694b33bead331ce1e3461d437
Brandon Dean <>
2017-06-27 17:34:31 -0400

    Add config to state and create API URL in the component

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -9,11 +9,42 @@ import { Spinner } from './common';
 class EventList extends Component {
 	state = {
 		events: [],
-		loading: true
+		loading: true,
+		config: Api.config,
 	}
 
 	componentWillMount() {
-		axios.get(Api.url)
+		this.getEventData();
+	}
+
+	createUrl() {
+		const {
+			ongoingEvents,
+			spacesReservations,
+			onlyEventsWithRegistration,
+			onlyFeaturedEvents,
+			ageGroups,
+			eventTypes
+		} = this.state.config;
+
+		let ag, et;
+
+		if (ageGroups.length > 0) {
+			console.log(ageGroups.map(group => `ageGroups=${group}`).join('&') + '&')
+			ag = ageGroups.map(group => `ageGroups=${group}`).join('&') + '&';
+		}
+
+		if (eventTypes.length > 0) {
+			et = eventTypes.map(type => `eventsTypes=${type}`).join('&') + '&';
+		}
+
+		return `https://demozonepublic.evanced.info/api/signup/eventlist?${ag}${et}isOngoingVisible=${ongoingEvents}&isSpacesReservationVisible=${spacesReservations}&onlyRegistrationEnabled=${onlyEventsWithRegistration}&onlyFeaturedEvents=${onlyFeaturedEvents}`
+	}
+
+	getEventData() {
+		const url = this.createUrl();
+
+		axios.get(url)
 			.then(response => {
 				console.log(response);
 				this.setState({
