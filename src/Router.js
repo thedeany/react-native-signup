36c0f135c15daf3ed807f0168aac2c74a54f2113
Brandon Dean <>
2017-06-27 17:36:33 -0400

    Add new `FilterView` scene and allow travel between scenes

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import Home from './components/Home';
+import FilterView from './components/FilterView';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="home" component={Home} title="Door Regional Library" />
+      <Scene
+        key="home"
+        component={Home}
+        title="Door Regional Library"
+        rightTitle="Filter"
+        onRight={() => Actions.filter()}
+      />
+      <Scene
+        key="filter"
+        component={FilterView}
+        title="Filter"
+        rightTitle="Apply"
+        onRight={() => {
+          console.log('applying filters...');
+          Actions.home({ type: 'reset' });
+        }}
+      />
     </Router>
   );
 };
