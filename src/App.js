a608c2bd49fc5b49534e1f301b406d1169b5432d
Brandon Dean <>
2017-06-29 18:28:55 -0400

    Add `redux`, `react-redux`, and `redux-thunk` and create store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { View } from 'react-native';
+import reducers from './reducers';
 import Router from './Router';
 
 class App extends Component {
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(thunk));
+
     return (
       <View style={{ flex: 1 }}>
-        <Router />
+        <Provider store={store}>
+          <Router />
+        </Provider>
       </View>
     );
   }
